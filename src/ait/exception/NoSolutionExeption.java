e22804c4e150cfddd40343be793e243efa61006d
Novikova Tatiana <tania.novikov@web.de>
2023-11-01 11:54:11 +0100

    beschreibung von change

---
+++ src/ait/exception/NoSolutionExeption.java
@@ -0,0 +1,9 @@
+package ait.exception;
+
+public class NoSolutionExeption extends Exception {
+    public NoSolutionExeption(){}
+    public NoSolutionExeption(String message){
+        super(message);
+    }
+
+}
---
+++ src/ait/exception/SolutionAnyNumberException.java
@@ -0,0 +1,10 @@
+package ait.exception;
+
+public class SolutionAnyNumberException extends RuntimeException {
+
+    public SolutionAnyNumberException(){}
+    public SolutionAnyNumberException(String message){
+        super(message);
+    }
+
+}
